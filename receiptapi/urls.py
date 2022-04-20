689221eaee6705bce2421a91969cc20599baa378
EC2 Default User <ec2-user@ip-172-31-25-203.eu-west-1.compute.internal>
2022-04-20 22:22:29 +0000

    block unwanted urls

--- receiptapi/urls.py
+++ receiptapi/urls.py
@@ -8,5 +8,5 @@ router.register(r'receipts', views.ReceiptViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('email/', views.email),
+#    path('email/', views.email),
 ]
